ee69a7725a8c68ee248e9ced8f19a754b1844de6
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-06-20 22:23:55 -0300

    Fantasma rosa adicionado e teste do pomo de ouro concluido

---
+++ Assets/Scripts/GameScripts/Ghosts/NavMesh/GoldNavMeshController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace GameScripts.Ghosts.NavMesh{
+    public class GoldNavMeshController : MonoBehaviour{
+        private NavMeshAgent _navMeshAgent;
+        public GameObject[] _allGameObjects;
+        private Vector2 _targetPosition;
+        public GridBehaviour Grid;
+        
+        
+        private void Awake() {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+
+            _navMeshAgent.updateRotation = false;
+            _navMeshAgent.updateUpAxis = false;
+        }
+
+        private void Update() {
+            List<Vector2> _allMaximumDistances = new List<Vector2>();
+            foreach (var obj in _allGameObjects) {
+                var maxDistance = 0f;
+                var maxPosition = new Vector2();
+                foreach (var cell in Grid.Grid.CellsMatrix) {
+                    var currentDistance = Vector2.Distance(obj.transform.position, cell.WorldPosition);
+                    if (currentDistance > maxDistance && cell.Walkable) {
+                        maxDistance = currentDistance;
+                        maxPosition = cell.WorldPosition;
+                    }
+                }
+                
+                _allMaximumDistances.Add(maxPosition);
+            }
+
+            float xPos = 0;
+            float yPos = 0;
+            foreach (var postion in _allMaximumDistances) {
+                xPos += postion.x;
+                yPos += postion.y;
+            }
+
+            xPos /= _allMaximumDistances.Count;
+            yPos /= _allMaximumDistances.Count;
+
+            xPos = Mathf.Clamp(xPos, Grid.Grid.CellsMatrix[0,0].WorldPosition.x,
+                Grid.Grid.CellsMatrix[31,31].WorldPosition.x);
+            
+            yPos = Mathf.Clamp(yPos, Grid.Grid.CellsMatrix[31,31].WorldPosition.y,
+                Grid.Grid.CellsMatrix[0,0].WorldPosition.y);
+
+            _targetPosition = new Vector2(xPos, yPos);
+            
+            _navMeshAgent.SetDestination(_targetPosition);
+        }
+    }
+}
---
+++ Assets/Scripts/GameScripts/Ghosts/NavMesh/PinkNavMeshHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = System.Random;
+
+namespace GameScripts.Ghosts.NavMesh{
+    public class PinkNavMeshHandler : MonoBehaviour{
+        private NavMeshAgent _navMeshAgent;
+        public GridBehaviour Grid;
+        private List<GridCell> _gridCellsList = new List<GridCell>();
+
+
+        private void Awake() {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+
+            _navMeshAgent.updateRotation = false;
+            _navMeshAgent.updateUpAxis = false;
+        }
+
+        private void Start() {
+            foreach (var cell in Grid.Grid.CellsMatrix) {
+                if(cell.Walkable)
+                    _gridCellsList.Add(cell);
+            }
+            
+            StartCoroutine(RandomPositionCO());
+        }
+
+        private IEnumerator RandomPositionCO() {
+            while(true){
+                Vector3 position;
+                
+                   
+                var random = new Random();
+                var index = random.Next(0, _gridCellsList.Count);
+
+
+                var cell = _gridCellsList[index];
+                position = cell.WorldPosition;
+                
+                _navMeshAgent.SetDestination(position);
+                
+                print(cell.GridPosition);
+
+                yield return new WaitForSeconds(6f);
+            }
+        }
+        
+    }
+}
