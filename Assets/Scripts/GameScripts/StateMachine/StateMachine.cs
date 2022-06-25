183610b8467cb60871e8acd51435f6b09068b1b7
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-06-25 09:55:22 -0300

    StateMachine base e base state implementados

---
+++ Assets/Scripts/GameScripts/StateMachine/StateMachine.cs
@@ -0,0 +1,23 @@
+﻿using GameScripts.StateMachine.States;
+using UnityEngine;
+
+namespace GameScripts.StateMachine{
+    public class StateMachine : MonoBehaviour{
+        [SerializeField] private BaseState _initialState;
+        private BaseState _currentState;
+
+        private void Awake() {
+            _currentState = _initialState;
+        }
+
+        public void ChangeState(BaseState nextState) {
+            _currentState.OnStateExit(this);
+            _currentState = nextState;
+            _currentState.OnStateEnter(this);
+        }
+
+        private void Update() {
+            _currentState.ExecuteState(this);
+        }
+    }
+}
---
+++ Assets/Scripts/GameScripts/StateMachine/States/BaseState.cs
@@ -0,0 +1,13 @@
+﻿using GameScripts.GameEvent;
+using UnityEngine;
+
+namespace GameScripts.StateMachine.States{
+    public abstract class BaseState : ScriptableObject{
+        public abstract void OnStateEnter(StateMachine stateMachine);
+        public abstract void OnStateExit(StateMachine stateMachine);
+        public abstract void ExecuteState(StateMachine stateMachine);
+        protected SOGameEvent _eventToRaise;
+        
+        
+    }
+}
