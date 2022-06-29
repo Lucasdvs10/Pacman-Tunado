92366f6d83b60f805c660292aa9dba1fbd2650b1
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-06-29 09:20:57 -0300

    renomeei o decider

--- Assets/Scripts/GameScripts/StateMachine/StateDeciders/SOAlwaysTrueDecider.cs
+++ Assets/Scripts/GameScripts/StateMachine/StateDeciders/SOAlwaysTrueDecider.cs
@@ -2,7 +2,7 @@
 
 namespace GameScripts.StateMachine.StateDeciders{
     [CreateAssetMenu(fileName = "AlwaysTrue", menuName = "StateMachine/Deciders/Always True", order = 0)]
-    public class AlwaysTrueDecider : SOBaseStateDecider{
+    public class SOAlwaysTrueDecgitider : SOBaseStateDecider{
         public override bool Decide(StateMachine stateMachine) {
             return true;
         }
