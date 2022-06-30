508d8edf11a43f5a879feded46f70ed5ca3f21e6
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-06-30 09:58:24 -0300

    Scatter mode implementado em todos os fantasmas

---
+++ Assets/Scripts/GameScripts/StateMachine/StateDeciders/SOIsAtPositionDecider.cs
@@ -0,0 +1,13 @@
+﻿using GameScripts.SOSingletons;
+using UnityEngine;
+
+namespace GameScripts.StateMachine.StateDeciders{
+    [CreateAssetMenu(fileName = "IsAtPosition", menuName = "StateMachine/Deciders/IsAtPosition")]
+    public class SOIsAtPositionDecider : SOBaseStateDecider{
+        [SerializeField] private SOSingleVector2Int _currentPosition;
+        [SerializeField] private SOSingleVector2Int _targetPosition;
+        public override bool Decide(StateMachine stateMachine) {
+            return _currentPosition.Value == _targetPosition.Value;
+        }
+    }
+}
