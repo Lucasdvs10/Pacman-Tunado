1677cce0d8e22f21b7b9681a0530d30e94cbd347
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-06-30 10:35:26 -0300

    Ao encostar no player, fantasmas e pomo de ouro disparam um GameEvent

---
+++ Assets/Scripts/GameScripts/Collisions/GhostCollidePlayerEventEmmiter.cs
@@ -0,0 +1,12 @@
+﻿using GameScripts.GameEvent;
+using UnityEngine;
+
+namespace GameScripts.Collisions{
+    [RequireComponent(typeof(CircleCollider2D))]
+    public class GhostCollidePlayerEventEmmiter : BaseEventEmmiter{
+        private void OnTriggerEnter2D(Collider2D col) {
+            if(col.tag == "Player")
+                InvokeEvent();
+        }
+    }
+}
