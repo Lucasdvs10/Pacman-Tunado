469b17ddff9c00a97464789f091638a91e8b6829
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-07-04 15:19:05 -0300

    Comidas são destruidas quando coletadas

---
+++ Assets/Scripts/GameScripts/DestroyCollisionWithPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GameScripts{
+    [RequireComponent(typeof(Collider2D))]
+    public class DestroyCollisionWithPlayer : MonoBehaviour{
+
+        private void OnTriggerEnter2D(Collider2D col) {
+            if(col.tag == "Player")
+                DestroyThisObject();
+        }
+
+        public void DestroyThisObject() {
+            Destroy(gameObject);
+        }
+        
+    }
+}
--- Assets/Scripts/GameScripts/Pontuation/PointsAdderOnPlayerCollision.cs
+++ Assets/Scripts/GameScripts/Pontuation/PointsAdderOnPlayerCollision.cs
@@ -1,28 +1,23 @@
-﻿using GameScripts.GameEvent;
-using GameScripts.SOSingletons;
+﻿using GameScripts.SOSingletons;
 using UnityEngine;
 
 namespace GameScripts.Pontuation{
-    public class PointsAdder : BaseEventEmmiter{
+    [RequireComponent(typeof(Collider2D))]
+    public class PointsAdderOnPlayerCollision : MonoBehaviour{
         [SerializeField] private SOSingleInt _pointsSingleton;
         [SerializeField] private int _pointsAmountValue;
-        [SerializeField] private SOGameEvent _event;
         
         private void Start() {
             _pointsSingleton.Value = 0;
         }
 
-        private void OnEnable() {
-            _event.Subscribe(AddAmount);
-        }
-
-        private void OnDisable() {
-            _event.Unsubscribe(AddAmount);
+        private void OnTriggerEnter2D(Collider2D col) {
+            if(col.tag == "Player")
+                AddAmount();
         }
 
         public void AddAmount() {
             _pointsSingleton.Value += _pointsAmountValue;
-            InvokeEvent();
         }
         
     }
