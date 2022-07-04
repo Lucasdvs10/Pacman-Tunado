6b302d9bc7d30c07b4f78d31400b35977fec3620
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-07-04 14:53:01 -0300

    Comida e singleton de pontos adicionados

---
+++ Assets/Scripts/GameScripts/Pontuation/PointsAdder.cs
@@ -0,0 +1,29 @@
+﻿using GameScripts.GameEvent;
+using GameScripts.SOSingletons;
+using UnityEngine;
+
+namespace GameScripts.Pontuation{
+    public class PointsAdder : BaseEventEmmiter{
+        [SerializeField] private SOSingleInt _pointsSingleton;
+        [SerializeField] private int _pointsAmountValue;
+        [SerializeField] private SOGameEvent _event;
+        
+        private void Start() {
+            _pointsSingleton.Value = 0;
+        }
+
+        private void OnEnable() {
+            _event.Subscribe(AddAmount);
+        }
+
+        private void OnDisable() {
+            _event.Unsubscribe(AddAmount);
+        }
+
+        public void AddAmount() {
+            _pointsSingleton.Value += _pointsAmountValue;
+            InvokeEvent();
+        }
+        
+    }
+}
