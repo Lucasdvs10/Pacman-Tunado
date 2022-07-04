50f378df6164c3bac2e7865e8a680ed6b730afd6
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-07-04 11:10:24 -0300

    agora o evento de colisão com o player só é invocado quando o fantasma está em determinados estados

--- Assets/Scripts/GameScripts/Collisions/GhostCollidePlayerEventEmmiter.cs
+++ Assets/Scripts/GameScripts/Collisions/GhostCollidePlayerEventEmmiter.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 namespace GameScripts.Collisions{
     [RequireComponent(typeof(CircleCollider2D))]
     public class GhostCollidePlayerEventEmmiter : BaseEventEmmiter{
+        [SerializeField] private SOGameEvent[] _stateEventEnterToListen;
+        [SerializeField] private SOGameEvent[] _stateEventExitToListen;
+        private bool _flag;
+
+        private void OnEnable() {
+            foreach (var state in _stateEventEnterToListen) {
+                state.Subscribe(TurnOnFlag);
+            }
+            
+            foreach (var state in _stateEventExitToListen) {
+                state.Subscribe(TurnOffFlag);
+            }
+        }
+
+        private void OnDisable() {
+            foreach (var state in _stateEventEnterToListen) {
+                state.Unsubscribe(TurnOnFlag);
+            }
+            
+            foreach (var state in _stateEventExitToListen) {
+                state.Unsubscribe(TurnOffFlag);
+            }
+        }
+
+        private void TurnOnFlag() => _flag = true;
+        private void TurnOffFlag() => _flag = false;
+
         private void OnTriggerEnter2D(Collider2D col) {
-            if(col.tag == "Player")
+            if(col.tag == "Player" && _flag)
                 InvokeEvent();
         }
     }
