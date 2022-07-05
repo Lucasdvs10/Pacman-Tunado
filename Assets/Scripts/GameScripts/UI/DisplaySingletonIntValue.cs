26913f2cd62094d8e1eea349532168fc1c2db810
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-07-05 15:49:15 -0300

    Começo do display de baterias feito

---
+++ Assets/Scripts/GameScripts/UI/DisplaySingletonIntValue.cs
@@ -0,0 +1,33 @@
+using System;
+using GameScripts.GameEvent;
+using GameScripts.SOSingletons;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class DisplaySingletonIntValue : MonoBehaviour{
+    [SerializeField] private SOSingleInt _singletonToDisplay;
+    [SerializeField] private SOGameEvent _event;
+    private TextMeshProUGUI _text;
+
+    private void Awake() {
+        _text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start() {
+    }
+
+    private void OnEnable() {
+        _event.Subscribe(UpdateText);
+        UpdateText();
+    }
+
+    private void OnDisable() {
+        _event.Unsubscribe(UpdateText);
+    }
+
+    private void UpdateText() {
+        _text.text = _singletonToDisplay.Value.ToString();
+    }
+    
+}
