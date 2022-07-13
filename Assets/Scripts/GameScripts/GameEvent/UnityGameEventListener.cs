8bf44da22c9f5e936516f4ab2ffdd4790bb4fd82
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-07-13 11:37:24 -0300

    Mudança de sprites quando entrar no estado morto foi implementada

---
+++ Assets/Scripts/GameScripts/GameEvent/UnityGameEventListener.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace GameScripts.GameEvent{
+    public class UnityGameEventListener : MonoBehaviour{
+        [SerializeField] private SOGameEvent _eventToListen;
+        public UnityEvent UnityEvent;
+
+        private void OnEnable() {
+            _eventToListen.Subscribe(InvokeEvent);
+        }
+
+        private void OnDisable() {
+            _eventToListen.Subscribe(InvokeEvent);
+        }
+
+        private void InvokeEvent() {
+            UnityEvent?.Invoke();
+        }
+    }
+}
