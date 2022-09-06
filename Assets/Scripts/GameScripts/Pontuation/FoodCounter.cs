16df9a683439f1a4c37db957006b4ecae1865c1a
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-09-06 16:29:27 -0300

    Contador de comida e decider que escuta multiplos eventos feito

---
+++ Assets/Scripts/GameScripts/Pontuation/FoodCounter.cs
@@ -0,0 +1,35 @@
+﻿using GameScripts.GameEvent;
+using GameScripts.SOSingletons;
+using UnityEngine;
+
+namespace GameScripts.Pontuation {
+    public class FoodCounter : BaseEventEmmiter {
+        [SerializeField] private SOSingleInt _foodRemainAmountSingleton;
+        [SerializeField] private SOGameEvent _eventToListen;
+
+        private void Start() {
+            GetFoodRemainAmount();
+            _foodRemainAmountSingleton.Value = GetFoodRemainAmount();
+        }
+
+        private void OnEnable() {
+            _eventToListen.Subscribe(DecreaseFoodRemainAmountSingleton);
+        }
+
+        private void OnDisable() {
+            _eventToListen.Unsubscribe(DecreaseFoodRemainAmountSingleton);
+        }
+
+        
+        public void DecreaseFoodRemainAmountSingleton() {
+            _foodRemainAmountSingleton.Value--;
+
+            if(_foodRemainAmountSingleton.Value <= 0)
+                InvokeEvent();
+        }
+        
+        private int GetFoodRemainAmount() {
+            return transform.childCount;
+        }
+    }
+}
---
+++ Assets/Scripts/GameScripts/StateMachine/StateDeciders/GameManager/MultipleEventsListenerDecider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using GameScripts.GameEvent;
+using UnityEngine;
+
+namespace GameScripts.StateMachine.StateDeciders.GameManager {
+    [CreateAssetMenu(fileName = "MultipleEventDecider", menuName = "StateMachine/Deciders/Multiple Event Listener")]
+    public class MultipleEventsListenerDecider : SOBaseStateDecider {
+        [SerializeField] private List<SOGameEvent> _eventsToListen;
+        private List<bool> _boolsList;
+
+        private void OnEnable() {
+            _boolsList = new List<bool>();
+            foreach (var gameEvent in _eventsToListen) {
+                _boolsList.Add(false);
+            }
+
+            for (int i = 0; i < _boolsList.Count; i++) {
+                _eventsToListen[i].Subscribe(() => SetIndexTrue(i));
+            }            
+        }
+
+        private void OnDisable() {
+            for (int i = 0; i < _eventsToListen.Count; i++) {
+                _eventsToListen[i].Unsubscribe(() => SetIndexTrue(i));
+            }
+        }
+
+
+        public override bool Decide(StateMachine stateMachine) {
+            foreach (var item in _boolsList) {
+                if (!item) return false;
+            }
+            return true;
+        }
+        
+        private void SetIndexTrue(int itemIndex) => _boolsList[itemIndex] = true;
+
+    }
+}
