40236f657ffc3d3af08a86a1c043e88a703671bb
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-09-08 17:43:48 -0300

    Escrevi a classe life controller usando TDD

---
+++ Assets/Scripts/GameScripts/LifeSystem/LifeController.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitTests {
+    public class LifeController {
+        private int _initialLifeAmount;
+        private int _currentLifeAmount;
+        
+        public int GetCurrentLife() => _currentLifeAmount;
+        public void ApplyDamage(int damageAmount) => _currentLifeAmount -= damageAmount;
+        
+        public LifeController(int initialLifeAmount) {
+            _initialLifeAmount = initialLifeAmount;
+            _currentLifeAmount = _initialLifeAmount;
+        }
+    }
+}
---
+++ Assets/Scripts/UnitTests/Life_Should.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests {
+    public class Life_Should {
+        private LifeController _lifeController;
+        
+        [SetUp]
+        public void Setup_Tests() {
+            _lifeController = new LifeController(3);
+        }
+
+        [Test]
+        public void Should_Return_3_When_Not_Taken_Damage() {
+            var currentLifeAmount = _lifeController.GetCurrentLife();
+            
+            Assert.AreEqual(3, currentLifeAmount);
+        }
+
+        [Test]
+        public void Should_Return_1_When_Taken_2_Damage() {
+            _lifeController.ApplyDamage(2);
+            
+            var currentLifeAmount = _lifeController.GetCurrentLife();
+
+            Assert.AreEqual(1, currentLifeAmount);
+        }
+        
+        [Test]
+        public void Should_Return_4_When_Taken_Negative_1_Damage() {
+            _lifeController.ApplyDamage(-1);
+            
+            var currentLifeAmount = _lifeController.GetCurrentLife();
+
+            Assert.AreEqual(4, currentLifeAmount);
+        }
+        
+        
+    }
+}
